8eda3c54e140b3a6ebc4be5c2c55e3cc8d1c50bf
Sergey Ivanov <ivanovknitting@gmail.com>
2023-07-13 16:03:28 +0300

    minimum run in ts as import express module

--- bin/app.js
+++ bin/app.js
@@ -1,3 +1,19 @@
-console.log("Hello app.js");
-
 require("../src/index.js");
+
+
+
+
+// const express = require('express');
+// const app = express();
+// const PORT = 3000;
+ 
+// app.get('/', function (req, res) {
+//     console.log(req.get('Accept'));
+//     console.log(req.accepts('text/plain'));
+//     res.end();
+// });
+ 
+// app.listen(PORT, function (err) {
+//     if (err) console.log(err);
+//     console.log("Server listening on PORT", PORT);
+// });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log("Hello SRC");
+import expressModule, { Express, Request, Response, NextFunction } from 'express';
+import concat from 'concat-stream';
+import os from 'node:os'
+
+const app: Express = expressModule();
+
+app.use(function (req: Request, res: Response, next: NextFunction) {
+	req.pipe(concat(function (data: Buffer): void {
+		req.body = data.toString('utf8');
+		next();
+	}));
+});
+
+app.all('*', function (req: Request, res: Response) {
+		const resultData: any = {
+		path: req.path,
+		headers: req.headers,
+		method: req.method,
+		body: req.body,
+		fresh: req.fresh,
+		hostname: req.hostname,
+		ip: req.ip,
+		protocol: req.protocol,
+		httpVersion: req.httpVersion,
+		query: req.query,
+		os: {
+			hostname: os.hostname()
+		},
+		client: {
+			localPort: req.socket.localPort,
+			remoteAddress: req.socket.remoteAddress,
+			remotePort: req.socket.remotePort,
+		}
+	};
+	const resultDataStr: string = JSON.stringify(resultData, null, '\t');
+	res.appendHeader("Content-Type", "application/json");
+	res.send(resultDataStr);
+})
+
+app.listen(3000)
